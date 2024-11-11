e0753ff47f7a66d36c22d2728ab87ac242c6553f
valpas312 <valenpalmas2005@gmail.com>
2024-11-10 21:33:37 -0300

    mas trabajo en la ui y logica en la edicion y eliminacion de workspaces

--- src/app/workspace/[workspaceId]/preferencesModal.tsx
+++ src/app/workspace/[workspaceId]/preferencesModal.tsx
@@ -1,12 +1,20 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useRemoveWorkspaces } from "@/features/workspaces/api/useRemoveWorkspace";
 import { useUpdateWorkspaces } from "@/features/workspaces/api/useUpdateWorkspace";
+import useWorkspaceId from "@/hooks/useWorkspaceId";
 import { TrashIcon } from "lucide-react";
 import { useState } from "react";
+import { toast } from "sonner";
 
 interface PreferencesModalProps {
   open: boolean;
@@ -19,10 +27,32 @@ export default function PreferencesModal({
   setOpen,
   initialValue,
 }: PreferencesModalProps) {
+  const workspaceId = useWorkspaceId();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
 
-  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } = useUpdateWorkspaces();
+  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
+    useUpdateWorkspaces();
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
+    useRemoveWorkspaces();
+
+  const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateWorkspace(
+      { id: workspaceId, name: value },
+      {
+        onSuccess: () => {
+          setEditOpen(false);
+          toast.success("Workspace updated");
+        },
+        onError: () => {
+          toast.error("Failed to update workspace");
+        },
+      }
+    );
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -31,15 +61,44 @@ export default function PreferencesModal({
           <DialogTitle>{value}</DialogTitle>
         </DialogHeader>
         <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <div className="px-5 py-4 bg-white roundedlg border cursor-pointer hover:bg-gray-50">
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-semibold">Workspace name</p>
-              <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                Edit
-              </p>
-            </div>
-            <p className="text-sm">{value}</p>
-          </div>
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="px-5 py-4 bg-white roundedlg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold">Workspace name</p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Edit
+                  </p>
+                </div>
+                <p className="text-sm">{value}</p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Rename this workspace</DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={handleEdit}>
+                <Input
+                  value={value}
+                  disabled={isUpdatingWorkspace}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={80}
+                  placeholder="Workspace name e.g. 'Marketing', 'Design', 'Engineering'"
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline" disabled={isUpdatingWorkspace}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button disabled={isUpdatingWorkspace}>Save</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
           <button
             disabled={false}
             onClick={() => {}}
